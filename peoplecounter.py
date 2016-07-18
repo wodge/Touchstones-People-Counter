8e5833a67af6a901890d8cb5b78bdbef9af901b9
Paul Woodhead <paul@noworriesit.co.uk>
2016-07-18 12:40:05 +0000

    variable id change

--- peoplecounter.py
+++ peoplecounter.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 try:
     api =ApiClient("e62234c8846318b2962b69c29b62a0b7bbd5ed18")
-    people = api.get_variable("wY36NjEfLRQ3Y1OlTmGLcm9zHKAVQk")
+    people = api.get_variable("578cc74b7625422c4e939b61")
 except:
         print("Couldn't connect to the API, check your Internet connection")
 counter = 0
